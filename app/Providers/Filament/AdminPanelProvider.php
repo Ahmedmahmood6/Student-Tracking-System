469a77216b11f34e6d7770c920c93bb4d38d1ba5
Ahmed <moahmedmahmoud333@gmail.com>
2026-09-13 14:49:57 +0300

    change name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->brandName('نظام متابعة الطلاب والتقارير')
+            ->brandName('نظام متابعة الطلاب')
             ->login()
             ->colors([
                 'primary' => Color::Pink,
